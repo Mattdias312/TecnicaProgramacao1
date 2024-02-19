e7ee23d6df0442a59ded76a7cdcdbb13ed3f9955
Mattdias312 <matheusf.dias12@gmail.com>
2024-02-19 20:59:10 -0300

    Aula01

---
+++ Pessoa/src/Classes/Aula1Pessoa.java
@@ -0,0 +1,17 @@
+
+package Classes;
+
+/**
+ *
+ * @author Alunos
+ */
+public class Aula1Pessoa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
