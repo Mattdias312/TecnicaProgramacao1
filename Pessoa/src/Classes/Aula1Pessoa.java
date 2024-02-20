2378a547d0044f3a639cd1d3a82fcc38cde9293f
Mattdias312 <matheusf.dias12@gmail.com>
2024-02-19 22:25:53 -0300

    Aula01

--- Pessoa/src/Classes/Aula1Pessoa.java
+++ Pessoa/src/Classes/Aula1Pessoa.java
@@ -11,7 +11,26 @@ public class Aula1Pessoa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Pessoa pai;
+        pai = new Pessoa();
+        pai.nome = "Marcelo";
+        pai.qtdFilhoAux = 2;
+        pai.qtdIrmaos = 4;
+        System.out.println(pai.Aprensentar());
+        
+        Pessoa mae = new Pessoa();
+        mae.nome = "Gisle";
+        mae.qtdFilhoAux = 3;
+        mae.qtdIrmaos = 1;
+        System.out.println(mae.Aprensentar());
+        
+        mae.nomeFilhos[0] = "Matheus";
+        mae.nomeFilhos[1] = "Gabriela";
+
+        System.out.println(mae.ApresentarFilhos());
+        
+        
     }
     
 }
--- Pessoa/src/Classes/Pessoa.java
+++ Pessoa/src/Classes/Pessoa.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Classes;
 
-/**
- *
- * @author Alunos
- */
 public class Pessoa {
+
+    //Atributos da classe
+    String nome;
+    int qtdFilhoAux, qtdIrmaos;
+    String[] nomeFilhos = new String[20];
+    
+    /*Método Apresentar, responde com o Nome 
+    concatenado com a quantidade de Filhos.
+    */
+    
+    public String Aprensentar(){
+        return nome + " possui " + qtdFilhoAux + " filhos!";
+    }
+    
+    public String ApresentarFilhos(){
+        String resposta = "Meus filhos são ";
+        int qtdFilhoAux = 0;
+        
+        for(int i=0;i<20;i++){
+        if(nomeFilhos[i] != null){
+            qtdFilhoAux++;
+           resposta +=  nomeFilhos[i] + ", ";
+            }
+        }
+        if(qtdFilhoAux > 0)
+            return resposta + "QTD Filhos: " + qtdFilhoAux;
+        else
+            return "Não tem filhos!";
+    }
     
 }
